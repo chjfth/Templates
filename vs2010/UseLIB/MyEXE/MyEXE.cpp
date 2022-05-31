ff13d54ba31086492a3b0fecfc29a57b3476d67c
chj Win7Evernote VM <chjfth@gmail.com>
2022-05-31 15:36:47 +0800

    UseLIB: Remove 'TCHAR' related thing, so that cpp code can be used in Linux as well.

--- vs2010/UseLIB/MyEXE/MyEXE.cpp
+++ vs2010/UseLIB/MyEXE/MyEXE.cpp
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <tchar.h>
 #include <locale.h>
 
 #include <MyAPI.h>
 
-int _tmain(int argc, TCHAR* argv[])
+int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "");
 	
 	MyAPI_version_st vst = {};
 	MyAPI_getversion(&vst);
 
-	_tprintf(_T("EXE: Using MyAPI(lib) version: %d.%d.%d.%d\n"), vst.major, vst.minor, vst.patch, vst.extra);
+	printf("EXE: Using MyAPI(lib) version: %d.%d.%d.%d\n", vst.major, vst.minor, vst.patch, vst.extra);
 
 	return 0;
 }
