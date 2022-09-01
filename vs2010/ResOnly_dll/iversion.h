25e6f29310a44c11ef11243be0176205ba7a0e97
chj Win7Evernote VM <chjfth@gmail.com>
2022-09-02 00:45:50 +0800

    Resource-only DLL template

---
+++ vs2010/ResOnly_dll/iversion.h
@@ -0,0 +1,24 @@
+#ifndef __MyAPI_iversion_h_
+#define __MyAPI_iversion_h_
+
+// This iversion.h is considered internal to MyAPI implementation.
+// API user should call MyAPI_getversion() to query its version at runtime.
+
+#define THISLIB_VMAJOR 1
+#define THISLIB_VMINOR 0
+#define THISLIB_VPATCH 0
+#define THISLIB_VEXTRA 0
+
+#define THISLIBstr__(n) #n
+#define THISLIBstr(n) THISLIBstr__(n)
+
+// The following 4 are used in .rc, 's' implies string.
+#define THISLIB_VMAJORs THISLIBstr(THISLIB_VMAJOR)
+#define THISLIB_VMINORs THISLIBstr(THISLIB_VMINOR)
+#define THISLIB_VPATCHs THISLIBstr(THISLIB_VPATCH)
+#define THISLIB_VEXTRAs THISLIBstr(THISLIB_VEXTRA)
+
+#define THISLIB_NAME "ResOnly"
+
+
+#endif
