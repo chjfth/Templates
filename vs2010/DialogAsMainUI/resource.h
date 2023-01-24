154920c3adc4aa330448aa877480f6bc15e454c9
chj Win7Evernote VM <chjfth@gmail.com>
2023-01-24 09:27:03 +0800

    Add WS_MINIMIZEBOX

--- vs2010/DialogAsMainUI/resource.h
+++ vs2010/DialogAsMainUI/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by DialogAsMainUI.rc
+// Used by DialogAsMainUI1.rc
 //
 #define IDI_WINMAIN                     100
 #define IDD_WINMAIN                     101
