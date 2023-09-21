6dda3796f4f480b0ae3c427a6c0d6e6dcdc0d76b
chjvm-multiVS <chjfth@gmail.com>
2023-09-21 11:52:09 +0800

    Add a sample GPU kernel sample function.

--- vs2010/-CUDA-/cuda_print/cuda_print.cu
+++ vs2010/-CUDA-/cuda_print/cuda_print.cu
@@ -4,6 +4,16 @@
 // Note: nvcc.exe will include necessary CUDA .h headers automatically,
 // so we do not have to #include them manually. (at least for CUDA 8.0)
 
+__global__ void mykernel() 
+{
+	// map from threadIdx/BlockIdx to pixel position
+	const int x = threadIdx.x + blockIdx.x * blockDim.x;
+	const int y = threadIdx.y + blockIdx.y * blockDim.y;
+	const int offset = x + y * blockDim.x * gridDim.x;
+
+	printf("mykernel at [%d, %d] offset=%d\n", x, y, offset);
+}
+
 extern"C" void 
 main_cuda_print(int argc, char* argv[])
 {
@@ -18,4 +28,6 @@ main_cuda_print(int argc, char* argv[])
 	{
 		printf("gpu_count = %d\n", gpu_count);
 	}
+	
+	mykernel<<<2, 3>>>();
 }
