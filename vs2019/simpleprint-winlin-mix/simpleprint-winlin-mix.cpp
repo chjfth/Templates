a39560549f57a0f98b87ff0597aaf7f8cf02bac3
Jimm Chen WDK10 <chjfth@gmail.com>
2023-12-27 21:51:50 +0800

    print sizeof(long)

--- vs2019/simpleprint-winlin-mix/simpleprint-winlin-mix.cpp
+++ vs2019/simpleprint-winlin-mix/simpleprint-winlin-mix.cpp
@@ -3,6 +3,7 @@
 int main(int argc, char *argv[])
 {
 	printf("sizeof(void*) = %d\n", (int)sizeof(void*));
+	printf("sizeof(long)  = %d\n", (int)sizeof(long));
 	printf("argc = %d\n", argc);
 	printf("argv[0] = %s\n", argv[0]);
 	
