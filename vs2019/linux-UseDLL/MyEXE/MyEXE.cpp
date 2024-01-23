97f33ab7574cbf7262abbb0b02c8241afcd23f77
Jimm Chen WDK10 <chjfth@gmail.com>
2024-01-24 00:36:32 +0800

    Supp to prev, enable WSL build. Add comment to MyEXE.cpp, guide the user how to set [Pre-Launch Command], so that the .so bundled EXE project can be debugged in VSIDE.

--- vs2019/linux-UseDLL/MyEXE/MyEXE.cpp
+++ vs2019/linux-UseDLL/MyEXE/MyEXE.cpp
@@ -4,6 +4,29 @@
 
 #include <MyAPI.h>
 
+/* === Debugging Linux exe with .so from Visual Studio, How To ===
+
+If you just load up this .vcxproj into VSIDE, F7 build(assume success), set a breakpoint
+at start of main(), and then F5 start debugging, the debugger definitely fails because
+this MyEXE.out does NOT know where to load the libMyAPI.so .
+
+This issue can be settled by setting a project-wide debugger option.
+
+Open this EXE's project-setting dialog, go to [Debugging] -> [Pre-Launch Command],
+fill in:
+
+	export LD_LIBRARY_PATH=$(vslinux_RemoteOutAbsDir)
+
+Launch F5 again, now the debugger should work and we see the debugger yellow arrow stop
+at the first line of main().
+
+The MSBuild variable vslinux_RemoteOutAbsDir is defined in this EXE's .vcxproj and it
+dynamically expands to where the .so and the .out resides. For each BuildConf|Platform
+combination you want to debug on, you should set that Pre-Launch Command once again.
+Tip: [Pre-Launch Command] settings are saved in the .vcxproj.user file by VSIDE.
+
+*/
+
 int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "");
