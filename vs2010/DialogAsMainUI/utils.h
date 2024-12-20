4f6c406c1e64d4120855d977b01c28896d175846
chj Win7Evernote VM <chjfth@gmail.com>
2024-12-20 20:00:47 +0800

    Update .vcxproj.user.template, make them exactly 600 bytes, no UTF8-BOM

--- vs2010/DialogAsMainUI/utils.h
+++ vs2010/DialogAsMainUI/utils.h
@@ -16,6 +16,7 @@ TCHAR* now_timestr(TCHAR buf[], int bufchars, bool ymd=false);
 BOOL vaMsgBox(HWND hwnd, UINT utype, const TCHAR *szTitle, const TCHAR *szfmt, ...);
 // -- utype: MB_OK|MB_ICONINFORMATION
 
+BOOL vaSetWindowText(HWND hwnd, const TCHAR *szfmt, ...);
 BOOL vaSetDlgItemText(HWND hwnd, int nIDDlgItem, const TCHAR *szfmt, ...);
 
 void vaDbgTs(const TCHAR *fmt, ...);
