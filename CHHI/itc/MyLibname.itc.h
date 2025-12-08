65ebf3dc532b9edba2eceb77eba9db8914e085bd
chj Win7Evernote VM <chjfth@gmail.com>
2025-12-08 17:35:00 +0800

    InterpretConst header template

---
+++ CHHI/itc/MyLibname.itc.h
@@ -0,0 +1,62 @@
+#ifndef __MyLibname_h_YYYYMMDD_
+#define __MyLibname_h_YYYYMMDD_
+
+
+/*
+
+extern CInterpretConst XXX;
+
+extern const SingleBit2Val_st _b2v_YYY[];
+extern const int N_b2v_YYY;
+extern CInterpretConst YYY;
+
+*/
+
+
+
+
+/*
+////////////////////////////////////////////////////////////////////////////
+ ___                 _                           _        _   _             
+|_ _|_ __ ___  _ __ | | ___ _ __ ___   ___ _ __ | |_ __ _| |_(_) ___  _ __  
+ | || '_ ` _ \| '_ \| |/ _ \ '_ ` _ \ / _ \ '_ \| __/ _` | __| |/ _ \| '_ \ 
+ | || | | | | | |_) | |  __/ | | | | |  __/ | | | || (_| | |_| | (_) | | | |
+|___|_| |_| |_| .__/|_|\___|_| |_| |_|\___|_| |_|\__\__,_|\__|_|\___/|_| |_|
+              |_|                                                           
+////////////////////////////////////////////////////////////////////////////
+*/
+// ++++++++++++++++++ Implementation Below ++++++++++++++++++
+//
+#if defined(MyLibname_IMPL) || (defined CHHI_ALL_IMPL && !defined CHHI_ALL_IMPL_HIDE_MyLibname) // [IMPL]
+
+
+/*
+
+Reference: My evclip 20250411.v1
+
+const Enum2Val_st _e2v_XXX[] =
+{
+    ITC_NAMEPAIR(NM_OUTOFMEMORY         ), // -1
+    ITC_NAMEPAIR(NM_CLICK               ), // -2
+    ITC_NAMEPAIR(NM_DBLCLK              ), // -3
+};
+CInterpretConst XXX(_e2v_XXX, ITCF_SINT);
+
+
+const SingleBit2Val_st _b2v_YYY[] =
+{
+    ITC_NAMEPAIR(DELETE),       //   (0x00010000L)
+    ITC_NAMEPAIR(READ_CONTROL), //   (0x00020000L)
+    ITC_NAMEPAIR(WRITE_DAC),    //   (0x00040000L)
+    ITC_NAMEPAIR(WRITE_OWNER),  //   (0x00080000L)
+    ITC_NAMEPAIR(SYNCHRONIZE),  //   (0x00100000L)
+};
+const int N_b2v_YYY = ARRAYSIZE(_b2v_YYY);
+CInterpretConst YYY(_b2v_YYY, ITCF_HEX4B);
+
+*/
+
+
+#endif // [IMPL]
+
+#endif // __MyLibname_h_YYYYMMDD_
