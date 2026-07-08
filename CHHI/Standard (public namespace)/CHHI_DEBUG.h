4f6f0fdfc99bf6781cfda620401f1433a79aba2d
chj Win7Evernote VM <chjfth@gmail.com>
2026-07-08 11:42:01 +0800

    minor hint update

--- CHHI/Standard (public namespace)/CHHI_DEBUG.h
+++ CHHI/Standard (public namespace)/CHHI_DEBUG.h
@@ -1,7 +1,7 @@
 #if 0
 
 // Actual developer can copy these to his own $prjdir/_devdbg/CHHI_DEBUG.h 
-// (except that change `#if 0` to `#if 1`).
+// (and remember to change `#if 0` to `#if 1`)
 
 #undef NDEBUG // always enable assert()
 
@@ -12,12 +12,15 @@
 #define vaDBG_MAX_LEVEL 2 // use 3 to see most verbose vaDBG output
 
 
-// concrete xxx_DEBUG macros for various CHHI libraries/modules
+// concrete xxx_DEBUG macros for various CHHI libraries/modules (examples):
 #define DigClock2_DEBUG
 #define Editbox_EnableKbdAdjustNumber_DEBUG
 #define SimpleIni_DEBUG
 #define SimpleIniEx_DEBUG
 #define DataXIni_DEBUG
+
+// User .cpp often need this:
+// #include <CHHI_vaDBG_is_vaDbgTs.h>
 
 
 #endif
